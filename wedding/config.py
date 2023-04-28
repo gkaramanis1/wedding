d60d0d7d854a74eaf91d30ac698803ed7c9554eb
George Karamanis <gkaramanis1@outlook.com>
2023-04-28 16:18:15 -0400

    config.py change

--- wedding/config.py
+++ wedding/config.py
@@ -2,4 +2,4 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://wedding:cngw@192.168.1.94:3306/wedding'#os.environ.get('SQLALCHEMY_DATABASE_URI')
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://wedding:cngw@localhost:3306/wedding'#os.environ.get('SQLALCHEMY_DATABASE_URI')
